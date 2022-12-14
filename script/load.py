2e518d2f04e5952c43a6e7e361552ea56f6758cb
Parveshbhatt <parveshbhatt118@gmail.com>
2022-12-14 19:59:24 +0530

    load file updated

--- script/load.py
+++ script/load.py
@@ -2,5 +2,7 @@
 import os
 
 def loadCheck():
-    if int(os.popen("top -n1 | awk '/Cpu\(s\):/ {print $2}'").readline().strip()) >= 20:
-        return False
+    if int(float(str(os.popen("top -n1 | awk '/Cpu\(s\):/ {print $2}'").readline().strip()))) >= 20:
+        return True
+
+        
